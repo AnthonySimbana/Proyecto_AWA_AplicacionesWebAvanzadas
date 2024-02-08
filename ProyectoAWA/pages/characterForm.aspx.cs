7615b9d0ae3a06e7b562dfa5a9e48bcf7c3fcc8d
Danny Cabrera <78981957+DonOtame@users.noreply.github.com>
2024-02-07 20:56:18 -0500

    Update project names

--- ProyectoAWA/pages/characterForm.aspx.cs
+++ ProyectoAWA/pages/characterForm.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace ProyectoAWA.pages
+namespace WebAppWarcraftFriends.pages
 {
     public partial class characterForm : System.Web.UI.Page
     {
